81091314031076741907ee737448fe1598a65c42
alexsvorada <alex@svorada.eu>
2024-10-31 10:26:44 +0100

    feat: add testimonial component

---
+++ components/Testimonial.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+	interface Props {
+		quote: string
+		author: string
+	}
+	const props = defineProps<Props>()
+	const id = props.author.toLowerCase().replace(' ', '-')
+</script>
+
+<template>
+	<section :id="id" class="flex flex-col items-center">
+		<img :src="`/${id}.jpg`" :alt="'Face of ' + props.author" width="128px" class="rounded-full" />
+		<blockquote>
+			<p>{{ props.quote }}</p>
+		</blockquote>
+		<cite>- {{ props.author }}</cite>
+	</section>
+</template>
+
+<style scoped></style>
