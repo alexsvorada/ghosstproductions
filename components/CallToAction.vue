bf1df80dac2342d4c61b1876e0c0147501e4b05d
alexsvorada <alex@svorada.eu>
2024-10-31 10:25:42 +0100

    feat: add calltoaction component

---
+++ components/CallToAction.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts"></script>
+
+<template>
+	<button
+		class="text-xl rounded-full font-bold drop-shadow-2xl bg-white text-black transition-colors duration-300 hover:bg-black hover:border-orange-400 hover:text-orange-400 border-solid border-4 border-white w-fit text-left px-4 py-2">
+		<p>Hire me!</p>
+	</button>
+	<nav class="mt-12 flex items-center justify-center gap-8" aria-label="Social media links">
+		<NuxtLink
+			to="https://www.instagram.com/luccas.ghosst"
+			class="text-gray-500 hover:text-orange-400 transition-colors duration-300"
+			aria-label="Follow on Instagram">
+			<Icon name="ri:instagram-line" size="48" />
+		</NuxtLink>
+		<NuxtLink
+			to="mailto:ghosstbusiness@gmail.com"
+			class="text-gray-500 hover:text-orange-400 transition-colors duration-300"
+			aria-label="Send email">
+			<Icon name="ri:mail-add-line" size="48" />
+		</NuxtLink>
+		<NuxtLink
+			to="https://x.com/Gh0ssT_"
+			class="text-gray-500 hover:text-orange-400 transition-colors duration-300"
+			aria-label="Follow on X (Twitter)">
+			<Icon name="ri:twitter-x-fill" size="48" />
+		</NuxtLink>
+	</nav>
+</template>
