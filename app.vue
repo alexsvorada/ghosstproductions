ee0bdf5cf316ff9b86803f1c761376995c23fff9
alexsvorada <alex@svorada.eu>
2024-10-31 10:28:25 +0100

    feat: implement basic structure

--- app.vue
+++ app.vue
@@ -1,52 +1,89 @@
 <script setup lang="ts">
 	import Testimonial from './components/Testimonial.vue'
-	import MetricCard from './components/MetricCard.vue'
+	import CallToAction from './components/CallToAction.vue'
+	import YoutubeMetrics from './components/YoutubeMetrics.vue'
 </script>
 
 <template>
-	<div class="min-h-screen flex flex-col border-2 border-blue-500">
-		<header class="w-full py-4 border-2 border-red-500 flex flex-col items-center">
-			<!-- Header content -->
-			<p>Luccas Ghosst</p>
-			<img src="/ghosst.png" alt="Logo of Ghosst Productions" width="71.5" />
+	<div class="min-h-screen w-screen flex flex-col items-center">
+		<header class="flex flex-col items-center">
+			<h1>Luccas - Ghosst Productions</h1>
+			<img src="/ghosst.png" alt="Logo of Ghosst Productions" width="128" class="shadow-lg" />
 			<p>Mastering Visual Success</p>
-			<p>Delivering high-impact videos that communicate complex concepts and captivate audiences</p>
+			<p><Icon name="ri:user-location-line" />Dubnica, Slovakia</p>
 		</header>
 
 		<main class="flex-1">
-			<section id="call-to-action" class="w-full py-12 border-2 border-yellow-500 flex flex-col items-center">
-				<button class="text-xl font-semibold">Let's start a project together! {iconPlaceholder}</button>
+			<section id="call-to-action" class="py-8 flex flex-col items-center">
+				<CallToAction />
 			</section>
 
-			<section id="hero" class="w-full py-12 border-2 border-green-500">
-				<div class="h-48 flex items-center justify-center">
-					<p class="text-gray-600">Hero Section</p>
+			<section aria-label="about section" class="py-8 flex flex-col gap-24 items-center">
+				<h2 aria-label="about heading" class="font-bold mb-8 text-orange-500 text-6xl">About Me</h2>
+				<div class="space-y-6 text-gray-400 text-left max-w-prose">
+					<p>
+						- I am an experienced Video Editor and Motion Designer with over 5 years of experience. I specialize in various
+						content types, from YouTube videos to TikToks, gameplays and more. My passion for video editing started years
+						ago and I have been improving my skills ever since.
+					</p>
+					<p>
+						- Formerly only an editor for a small live-streamer. Editing only in my free time for free since high-school
+						until my channel gained 28,000+ subscribers and over 19,500,000 views over the years.
+					</p>
+					<p>
+						- I bring that experience and expertise to every project I undertake. I am creative and responsible. I like to
+						learn new things and skills. I have been working in the field of editing for over 5 years and I am constantly
+						trying to improve.
+					</p>
 				</div>
 			</section>
 
-			<section id="about-me" class="w-full py-12 border-2 border-purple-500">
-				<div class="h-48 flex items-center justify-center">
-					<p class="text-gray-600">About Me Section</p>
-				</div>
+			<section aria-label="metrics section" class="py-8 flex flex-col gap-24 items-center">
+				<h2 aria-label="metrics heading" class="font-bold mb-8 text-orange-500 text-6xl">Projects I worked on</h2>
+				<YoutubeMetrics channel-name="Gh0ssT Highlights" channel-id="UCdKv-5cDhkrkUxQnV1VuHpQ" />
+				<YoutubeMetrics channel-name="Krabs04" channel-id="UCnkJW9eS4y-0f6lrcpkJatA" />
+				<YoutubeMetrics channel-name="Grayson Loupas" channel-id="UC9QYz22gCEb9-mYmZn9d4aQ" />
 			</section>
 
-			<section id="metrics" class="w-full py-12 border-2 border-orange-500">
-				<div class="container mx-auto flex flex-wrap justify-center gap-6">
-					<MetricCard :metric="100">Projects completed</MetricCard>
-					<MetricCard :metric="20">M views</MetricCard>
-					<MetricCard :metric="5">Years of experience</MetricCard>
-				</div>
-			</section>
-
-			<section id="testimonials" class="w-full py-12 border-2 border-pink-500">
-				<div class="container mx-auto flex flex-col gap-6">
+			<section aria-label="testimonial section" class="py-8 flex flex-col gap-24 items-center">
+				<h2 aria-label="testimonial heading" class="font-bold mb-8 text-orange-500 text-6xl">Testimonials</h2>
+				<div class="space-y-4">
 					<Testimonial quote="Just watched the video. Freaking crushed it!" author="Grayson Loupas" />
+				</div>
+
+				<div class="space-y-4">
 					<Testimonial quote="Super good edit on this video, thank you." author="Dan Suchomel" />
+				</div>
+
+				<div class="flex items-center justify-center">
 					<Testimonial quote="Amazing work with the latest video!" author="Clay Erickson" />
+				</div>
+			</section>
+
+			<section aria-label="work overview section" class="py-8 flex flex-col gap-24 items-center">
+				<h2 aria-label="work overview heading" class="font-bold mb-8 text-orange-500 text-6xl">Work Overview</h2>
+				<div class="space-y-4">
+					<video
+						src="/grayson-loupas.webm"
+						autoplay
+						loop
+						muted
+						playsinline
+						class="w-full rounded-lg shadow-lg border border-orange-900/50 hover:border-orange-500/50 transition-colors duration-300"
+						aria-label="Video preview for Grayson Loupas project" />
+				</div>
+
+				<div class="space-y-4">
+					<video
+						src="/dan-suchomel.webm"
+						autoplay
+						loop
+						muted
+						playsinline
+						class="w-full rounded-lg shadow-lg border border-orange-900/50 hover:border-orange-500/50 transition-colors duration-300"
+						aria-label="Video preview for Dan Suchomel project" />
 				</div>
 			</section>
 		</main>
 	</div>
 </template>
-
-<style scoped></style>
