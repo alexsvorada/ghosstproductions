202bd3b82511cbbc03d12ca4e85b77b22898b95b
alexsvorada <alex@svorada.eu>
2024-11-07 23:32:48 +0100

    feat: add call to action section

--- components/CallToAction.vue
+++ components/CallToAction.vue
@@ -2,10 +2,10 @@
 
 <template>
 	<button
-		class="text-xl rounded-full font-bold drop-shadow-2xl bg-white text-black transition-colors duration-300 hover:bg-black hover:border-orange-400 hover:text-orange-400 border-solid border-4 border-white w-fit text-left px-4 py-2">
+		class="text-xl rounded-full font-bold drop-shadow-2xl bg-white text-black transition-colors duration-300 hover:bg-black hover:border-orange-400 hover:text-orange-400 border-solid border-4 border-white text-center px-4 py-2">
 		<p>Hire me!</p>
 	</button>
-	<nav class="mt-12 flex items-center justify-center gap-8" aria-label="Social media links">
+	<nav class="flex items-center" aria-label="Social media links">
 		<NuxtLink
 			to="https://www.instagram.com/luccas.ghosst"
 			class="text-gray-500 hover:text-orange-400 transition-colors duration-300"
