bfda93b2df620728e6e15c311e93e6673466118f
alexsvorada <alex@svorada.eu>
2024-11-09 20:37:38 +0100

    feat: impl VideoPreview component

---
+++ components/VideoPreview.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+	type Props = {
+		source: string
+		label: string
+	}
+
+	const props = defineProps<Props>()
+</script>
+
+<template>
+	<video
+		:src="props.source"
+		autoplay
+		loop
+		muted
+		playsinline
+		class="w-full rounded-lg shadow-lg border border-orange-900/50 hover:border-orange-500/50 transition-colors duration-300"
+		:aria-label="props.label" />
+</template>
