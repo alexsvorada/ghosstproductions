c9439e3d74d0badc43a50761072d4ac6d2e69a28
alexsvorada <alex@svorada.eu>
2024-11-09 20:36:48 +0100

    feat: add nuxt image module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/icon', '@nuxt/fonts']
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/icon',
+    '@nuxt/fonts',
+    '@nuxt/image'
+  ]
 })
